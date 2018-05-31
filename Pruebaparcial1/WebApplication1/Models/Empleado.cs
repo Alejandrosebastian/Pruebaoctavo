af9cef9889130f7dd0e0eba51b429483d8c7ad42
VictoriaMaria1707 <38673421+VictoriaMaria1707@users.noreply.github.com>
2018-05-31 12:28:18 -0500

    tabla contacto

--- Pruebaparcial1/WebApplication1/Models/Empleado.cs
+++ Pruebaparcial1/WebApplication1/Models/Empleado.cs
@@ -8,7 +8,7 @@ namespace WebApplication1.Models
 {
     public class Empleado
     {
-        public int IdContactos { get; set; }
+        public int EmpleadoId { get; set; }
         [Required(ErrorMessage = "Campo Requerido")]
         [StringLength(10, MinimumLength = 0, ErrorMessage = "la cedula solo tiene 10 caracter")]
         public int Cedula { get; set; }
@@ -24,8 +24,7 @@ namespace WebApplication1.Models
         [Required(ErrorMessage = "Campo Requerido")]
         [StringLength(19, MinimumLength = 0, ErrorMessage = "El Telefono tiene que tener maximo 16 caracteres")]
         public int Telefono { get; set; }
-   
-        public string Email { get; set; }
+          public string Email { get; set; }
         [Required(ErrorMessage = "Campo Requerido")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "El tipo de gabinete tiene que tener minimo 4 caracteres")]
         public string Tipo_gabinete { get; set; }
